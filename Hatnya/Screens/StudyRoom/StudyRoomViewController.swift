9808459b894b65e6975765b9f75b48b3e4627631
chemi <wbsldj7645@naver.com>
2022-07-19 00:00:52 +0900

    [ADD] navigation, taskLabel 추가 (#7)

--- Hatnya/Screens/StudyRoom/StudyRoomViewController.swift
+++ Hatnya/Screens/StudyRoom/StudyRoomViewController.swift
@@ -9,8 +9,39 @@ import UIKit
 
 class StudyRoomViewController: UIViewController {
 
+    // MARK: - property
+
+    private let navigationBarRightItem: UIBarButtonItem = {
+        let item = UIBarButtonItem()
+        item.image = UIImage(systemName: "ellipsis")
+        return item
+    }()
+    private let everyTaskLabel: UILabel = {
+        let label = UILabel()
+        label.text = "모두의 숙제 현황"
+        return label
+    }()
+
+    // MARK: - life cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        configUI()
+        render()
     }
-    
+
+    private func configUI() {
+        view.backgroundColor = .white
+        title = "Swift Study"
+        navigationItem.rightBarButtonItem = navigationBarRightItem
+    }
+
+    private func render() {
+        view.addSubview(everyTaskLabel)
+        everyTaskLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            everyTaskLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 38),
+            everyTaskLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20)
+            ])
+    }
 }
