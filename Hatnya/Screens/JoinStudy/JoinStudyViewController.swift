190cd08da13ab29e1232f42a094a43bc7f337256
Seungwon <csw98213@gmail.com>
2022-07-20 04:56:50 +0900

    [FEAT] JoinStudyView UI 생성

--- Hatnya/Screens/JoinStudy/JoinStudyViewController.swift
+++ Hatnya/Screens/JoinStudy/JoinStudyViewController.swift
@@ -8,8 +8,15 @@
 import UIKit
 
 class JoinStudyViewController: UIViewController {
-
+    
+    @IBOutlet private var textField: UITextField!
+    @IBOutlet private var searchResultView: UIView!
+    @IBOutlet private var studyGroupName: UILabel!
+    @IBOutlet private var studyGroupDescription: UILabel!
+    @IBOutlet private var nextButton: UIButton!
+    
     override func viewDidLoad() {
+        searchResultView.layer.cornerRadius = 5
         super.viewDidLoad()
     }
 
