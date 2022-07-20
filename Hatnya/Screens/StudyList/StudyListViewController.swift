a52c632f153dab8e7b49324e21f6f007d22396c5
wonhui-kim <0512tini@gmail.com>
2022-07-20 20:05:58 +0900

    [ADD] table view, cell 생성 후 sample data 보여주기 (#19)

--- Hatnya/Screens/StudyList/StudyListViewController.swift
+++ Hatnya/Screens/StudyList/StudyListViewController.swift
@@ -14,3 +14,18 @@ class StudyListViewController: UIViewController {
     }
     
 }
+
+extension StudyListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return StudyGroup.sampleData.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "studyListCell", for: indexPath)
+        
+        cell.textLabel?.text = StudyGroup.sampleData[indexPath.row].studyName
+        cell.detailTextLabel?.text = StudyGroup.sampleData[indexPath.row].description
+        
+        return cell
+    }
+}
