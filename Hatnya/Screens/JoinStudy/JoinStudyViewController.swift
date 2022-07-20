24f6af8d819431d9cf9f790b30c9b8bb143ab560
Seungwon <csw98213@gmail.com>
2022-07-20 10:33:50 +0900

    [FEAT] textField 글자수 제한 및 nextButton 글자수에 따른 색 변경

--- Hatnya/Screens/JoinStudy/JoinStudyViewController.swift
+++ Hatnya/Screens/JoinStudy/JoinStudyViewController.swift
@@ -7,17 +7,49 @@
 
 import UIKit
 
-class JoinStudyViewController: UIViewController {
+class JoinStudyViewController: UIViewController, UITextFieldDelegate {
     
-    @IBOutlet private var textField: UITextField!
+    @IBOutlet private var codeTextField: UITextField!
     @IBOutlet private var searchResultView: UIView!
     @IBOutlet private var studyGroupName: UILabel!
     @IBOutlet private var studyGroupDescription: UILabel!
     @IBOutlet private var nextButton: UIButton!
     
-    override func viewDidLoad() {
-        searchResultView.layer.cornerRadius = 5
-        super.viewDidLoad()
+    func searchStudyGroup() {
+        print("searching study group")
+    }
+    
+    func checkMaxLength(textField: UITextField!, maxLength: Int) {
+        if(textField.text?.count ?? 0 == maxLength) {
+            searchStudyGroup()
+            nextButton.isEnabled = true
+        } else if (textField.text?.count ?? 0 > maxLength) {
+            textField.deleteBackward()
+        } else {
+            nextButton.isEnabled = false
+        }
     }
 
+    @IBAction private func touchUpNextButton(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction private func touchUpDismissButton(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction private func textDidChanged(_ sender: UITextField) {
+        checkMaxLength(textField: codeTextField, maxLength: 6)
+    }
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        codeTextField.delegate = self
+        searchResultView.layer.cornerRadius = 5
+        nextButton.isEnabled = false
+        nextButton.setBackgroundColor(.systemGray6, for: .disabled)
+    }
+    
 }
