be23bef7243c965e25b26e45f5492ed6303d47f7
Seungwon <csw98213@gmail.com>
2022-07-21 10:22:27 +0900

    [FEAT] TextField underlined 적용 및 코드리뷰 반영

--- Hatnya/Screens/JoinStudy/JoinStudyViewController.swift
+++ Hatnya/Screens/JoinStudy/JoinStudyViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class JoinStudyViewController: UIViewController, UITextFieldDelegate {
+class JoinStudyViewController: UIViewController {
     
     @IBOutlet private var codeTextField: UITextField!
     @IBOutlet private var searchResultView: UIView!
@@ -15,7 +15,7 @@ class JoinStudyViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet private var studyGroupDescription: UILabel!
     @IBOutlet private var nextButton: UIButton!
     
-    func searchStudyGroup() {
+    private func searchStudyGroup() {
         for studyGroup in StudyGroup.sampleData {
             if codeTextField.text == studyGroup.code {
                 searchResultView.backgroundColor = .systemGray6
@@ -28,20 +28,6 @@ class JoinStudyViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    func checkMaxLength(textField: UITextField!, maxLength: Int) {
-        if textField.text?.count ?? 0 == maxLength {
-            searchStudyGroup()
-            nextButton.isEnabled = true
-        } else if textField.text?.count ?? 0 > maxLength {
-            textField.deleteBackward()
-        } else {
-            nextButton.isEnabled = false
-            studyGroupName.text = ""
-            studyGroupDescription.text = ""
-            searchResultView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
-        }
-    }
-
     @IBAction private func touchUpNextButton(_ sender: UIButton) {
         
     }
@@ -62,6 +48,47 @@ class JoinStudyViewController: UIViewController, UITextFieldDelegate {
         searchResultView.layer.cornerRadius = 5
         nextButton.isEnabled = false
         nextButton.setBackgroundColor(.systemGray6, for: .disabled)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        setTextField()
+    }
+    
+}
+
+extension UITextField {
+    
+    func underlined(viewSize: CGFloat, color: UIColor) {
+        let border = CALayer()
+        let width = CGFloat(1)
+        border.borderColor = color.cgColor
+        border.frame = CGRect(x: 0, y: self.frame.size.height + 10, width: viewSize - 40, height: width)
+        border.borderWidth = width
+        self.layer.addSublayer(border)
+    }
+    
+}
+
+extension JoinStudyViewController: UITextFieldDelegate {
+    private func checkMaxLength(textField: UITextField!, maxLength: Int) {
+        if textField.text?.count ?? 0 == maxLength {
+            searchStudyGroup()
+            nextButton.isEnabled = true
+        } else if textField.text?.count ?? 0 > maxLength {
+            textField.deleteBackward()
+        } else {
+            nextButton.isEnabled = false
+            studyGroupName.text = ""
+            studyGroupDescription.text = ""
+            searchResultView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0)
+        }
+    }
+    
+    private func setTextField() {
+        codeTextField.clearButtonMode = .whileEditing
+        codeTextField.underlined(viewSize: UIScreen.main.bounds.width, color: .systemGray)
     }
     
 }
