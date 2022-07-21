1f8603e29d468fa2710d1ab69c5b2a0f842e19f1
Lia316 <kjk0825@postech.ac.kr>
2022-07-21 22:34:14 +0900

    [FEAT] configure editTaskTableView with mock data (#9)

--- Hatnya/Screens/EditTask/EditTaskViewController.swift
+++ Hatnya/Screens/EditTask/EditTaskViewController.swift
@@ -9,9 +9,58 @@ import SwiftUI
 import UIKit
 
 class EditTaskViewController: UIViewController {
-
+    
+    private var taskDataSource: UITableViewDiffableDataSource<EditTaskSection, Homework>!
+    private var snapshot = NSDiffableDataSourceSnapshot<EditTaskSection, Homework>()
+    private var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        configureLayout()
+        configureDatasource()
+        applySnapshot()
+    }
+    
+}
+
+private enum EditTaskSection {
+    case main
+}
+
+private class EditTaskDatasource: UITableViewDiffableDataSource<EditTaskSection, Homework> {
+    
+}
+
+extension EditTaskViewController {
+    
+    private func configureLayout() {
+        tableView = UITableView(frame: .zero, style: .plain)
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: UITableViewCell.description())
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+    private func configureDatasource() {
+        taskDataSource = EditTaskDatasource(tableView: tableView, cellProvider: { tableView, indexPath, task in
+            let cell = tableView.dequeueReusableCell(withIdentifier: UITableViewCell.description(), for: indexPath)
+            var content = cell.defaultContentConfiguration()
+            content.text = task.name
+            cell.contentConfiguration = content
+            return cell
+        })
+    }
+    
+    private func applySnapshot() {
+        snapshot.appendSections([.main])
+        snapshot.appendItems(HomeworkMockData.longList, toSection: .main)
+        taskDataSource.apply(snapshot)
     }
     
 }
