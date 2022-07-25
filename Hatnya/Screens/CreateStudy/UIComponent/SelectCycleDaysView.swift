49be272aaf3d5e087fde0cf2be46383da8a6350c
kelly1422 <kelly1422@naver.com>
2022-07-26 01:23:20 +0900

    [CHORE] 리뷰반영 - 주석 삭제 (#13)

--- Hatnya/Screens/CreateStudy/UIComponent/SelectCycleDaysView.swift
+++ Hatnya/Screens/CreateStudy/UIComponent/SelectCycleDaysView.swift
@@ -11,15 +11,6 @@ class SelectCycleDaysView: UIView {
     lazy var cyclePickerView = CyclePickerView()
     lazy var dayButtonStackView = DayButtonStackView()
     
-//    2차 스프린트 ToDo - 선택한 주기 실시간으로 업데이트해서 표시
-//    let selectedCycleDaysLabel: UILabel = {
-//        let label = UILabel()
-//        label.text = "숙제가 매주 월요일 및 목요일에 반복됩니다."
-//        label.font = UIFont.systemFont(ofSize: 14)
-//        label.textColor = .systemGray2
-//        return label
-//    }()
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
@@ -49,58 +40,6 @@ class SelectCycleDaysView: UIView {
             dayButtonStackView.centerXAnchor.constraint(equalTo: cyclePickerView.centerXAnchor),
             dayButtonStackView.bottomAnchor.constraint(equalTo: self.bottomAnchor)
         ])
-        
-//    2차 스프린트 ToDo - 선택한 주기 실시간으로 업데이트해서 표시
-//        addSubview(selectedCycleDaysLabel)
-//        selectedCycleDaysLabel.translatesAutoresizingMaskIntoConstraints = false
-//        NSLayoutConstraint.activate([
-//            selectedCycleDaysLabel.topAnchor.constraint(equalTo: dayButtonStackView.bottomAnchor, constant: 20),
-//            selectedCycleDaysLabel.leadingAnchor.constraint(equalTo: dayButtonStackView.leadingAnchor),
-//            selectedCycleDaysLabel.centerXAnchor.constraint(equalTo: dayButtonStackView.centerXAnchor),
-//            selectedCycleDaysLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor)
-//        ])
     }
     
 }
-
-//    2차 스프린트 ToDo - 선택한 주기 실시간으로 업데이트해서 표시
-
-//extension SelectCycleDaysView: UIGestureRecognizerDelegate {
-//    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
-//        var completeSentence = ""
-//        let selectedCycle = cyclePickerView.selectedCycleLabel.text ?? ""
-//
-//        var days: [String] = []
-//
-//        if dayButtonStackView.sundayButton.isSelected {
-//            days.append("일")
-//        }
-//        if dayButtonStackView.mondayButton.isSelected {
-//            days.append("월")
-//        }
-//        if dayButtonStackView.tuesdayButton.isSelected {
-//            days.append("화")
-//        }
-//        if dayButtonStackView.wednesdayButton.isSelected {
-//            days.append("수")
-//        }
-//        if dayButtonStackView.thursdayButton.isSelected {
-//            days.append("목")
-//        }
-//        if dayButtonStackView.fridayButton.isSelected {
-//            days.append("금")
-//        }
-//        if dayButtonStackView.saturdayButton.isSelected {
-//            days.append("토")
-//        }
-//
-//        completeSentence = "숙제가 " + selectedCycle + "마다 "
-//        days.forEach { day in
-//            completeSentence = completeSentence + day + ", "
-//        }
-//        completeSentence = completeSentence + "에 반복됩니다."
-//        selectedCycleDaysLabel.text = completeSentence
-//        return true
-//    }
-//
-//}
