994a7dcf2ba02c1aa3dbf7dda0c5e9d87fa54dd0
chemi <wbsldj7645@naver.com>
2022-07-27 15:37:48 +0900

    [CHORE] sceneDelegate 수정(#30)

--- Hatnya/Global/Supports/SceneDelegate.swift
+++ Hatnya/Global/Supports/SceneDelegate.swift
@@ -11,11 +11,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
-        let rootViewControoler = UINavigationController(rootViewController: StudyRoomViewController())
-        window?.rootViewController = rootViewControoler
-        window?.makeKeyAndVisible()
     }
 
 }
