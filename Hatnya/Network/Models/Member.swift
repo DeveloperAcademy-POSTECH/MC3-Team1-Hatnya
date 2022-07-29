34456553fb6081dea3060d93584630f4dbf9367f
chemi <wbsldj7645@naver.com>
2022-07-29 16:04:16 +0900

    [ADD] Member uid 변수 추가 (#30)

--- Hatnya/Network/Models/Member.swift
+++ Hatnya/Network/Models/Member.swift
@@ -8,43 +8,44 @@
 import Foundation
 
 struct Member {
+    let uid: String
     let nickname: String
     let homeworks: [Homework]
 }
 
 extension Member {
     static let testMemberList: [Member] = [
-        Member(nickname: "Chemi", homeworks: [
+        Member(uid: "", nickname: "Chemi", homeworks: [
             Homework(name: "알고리즘 1000번 문제", due: nil, isCompleted: false),
             Homework(name: "알고리즘 2000번 문제", due: nil, isCompleted: true),
             Homework(name: "알고리즘 3000번 문제", due: nil, isCompleted: false),
             Homework(name: "알고리즘 4000번 문제", due: nil, isCompleted: true)
         ]),
-        Member(nickname: "Zero", homeworks: [
+        Member(uid: "", nickname: "Zero", homeworks: [
             Homework(name: "알고리즘 1000번 문제", due: nil, isCompleted: false),
             Homework(name: "알고리즘 2000번 문제", due: nil, isCompleted: true),
             Homework(name: "알고리즘 3000번 문제", due: nil, isCompleted: true),
             Homework(name: "알고리즘 4000번 문제", due: nil, isCompleted: false)
         ]),
-        Member(nickname: "Lia", homeworks: [
+        Member(uid: "", nickname: "Lia", homeworks: [
             Homework(name: "알고리즘 1000번 문제", due: nil, isCompleted: true),
             Homework(name: "알고리즘 2000번 문제", due: nil, isCompleted: false),
             Homework(name: "알고리즘 3000번 문제", due: nil, isCompleted: false),
             Homework(name: "알고리즘 4000번 문제", due: nil, isCompleted: true)
         ]),
-        Member(nickname: "May", homeworks: [
+        Member(uid: "", nickname: "May", homeworks: [
             Homework(name: "알고리즘 1000번 문제", due: nil, isCompleted: true),
             Homework(name: "알고리즘 2000번 문제", due: nil, isCompleted: true),
             Homework(name: "알고리즘 3000번 문제", due: nil, isCompleted: false),
             Homework(name: "알고리즘 4000번 문제", due: nil, isCompleted: false)
         ]),
-        Member(nickname: "Eve", homeworks: [
+        Member(uid: "", nickname: "Eve", homeworks: [
             Homework(name: "알고리즘 1000번 문제", due: nil, isCompleted: false),
             Homework(name: "알고리즘 2000번 문제", due: nil, isCompleted: false),
             Homework(name: "알고리즘 3000번 문제", due: nil, isCompleted: true),
             Homework(name: "알고리즘 4000번 문제", due: nil, isCompleted: false)
         ]),
-        Member(nickname: "Bit", homeworks: [
+        Member(uid: "", nickname: "Bit", homeworks: [
             Homework(name: "알고리즘 1000번 문제", due: nil, isCompleted: true),
             Homework(name: "알고리즘 2000번 문제", due: nil, isCompleted: true),
             Homework(name: "알고리즘 3000번 문제", due: nil, isCompleted: true),
