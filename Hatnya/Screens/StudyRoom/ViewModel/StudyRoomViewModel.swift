eb7f662e14c2a9683cc8c554464aa24d8caece02
chemi <wbsldj7645@naver.com>
2022-07-30 17:37:07 +0900

    [FEAT] 현재 회차 조회 후 데이터 fetch (#50)

--- Hatnya/Screens/StudyRoom/ViewModel/StudyRoomViewModel.swift
+++ Hatnya/Screens/StudyRoom/ViewModel/StudyRoomViewModel.swift
@@ -15,15 +15,48 @@ struct HomeworkTask: Codable {
     var list: [Homework]
 }
 
+struct FirebaseStudyInfo: Codable {
+    let name: String
+    let code: String
+    let createdAt: Date
+    let description: String
+}
+
 class StudyRoomViewModel {
+    @Published var currentCount: Int = 0
     @Published var userTaskList: [Member] = []
 
     init(studyUid: String) {
-        fetchMemberList(studyUid: studyUid, cycle: 3)
+        fetchCurrentStudyCount(studyUid: studyUid)
     }
-    
+
     func clearUserTaskList() {
         userTaskList.removeAll()
+    }
+
+    func fetchCurrentStudyCount(studyUid: String) {
+        let firestoreDb = Firestore.firestore()
+        firestoreDb
+            .collection("StudyGroup")
+            .document(studyUid)
+            .getDocument(as: FirebaseStudyInfo.self, completion: { result in
+            switch result {
+            case .success(let info):
+                self.currentCount = self.getCurrentStudyCount(to: info.createdAt)
+                print("self.currentCount = \(self.currentCount)")
+                self.fetchMemberList(studyUid: studyUid, cycle: 3)
+            case .failure(let err):
+                print(err)
+            }
+        })
+    }
+    
+    func getCurrentStudyCount(to startDate: Date) -> Int {
+        let oneDayTimeInterval: TimeInterval = 86_400
+        let distance = startDate.distance(to: Date())
+        let distanceDay = Int(distance / oneDayTimeInterval)
+        let count = distanceDay / 7
+        return count + 1
     }
 
     func fetchMemberList(studyUid: String, cycle: Int) {
@@ -42,7 +75,7 @@ class StudyRoomViewModel {
                     let uid = data["uid"] as? String ?? ""
                     let name = data["nickname"] as? String ?? ""
                     document.reference.collection("Homeworks")
-                        .whereField("cycle", isEqualTo: cycle)
+                        .whereField("cycle", isEqualTo: self.currentCount)
                         .getDocuments { querySnapshot, err in
                         if let err = err {
                             print("Error getting documents: \(err)")
