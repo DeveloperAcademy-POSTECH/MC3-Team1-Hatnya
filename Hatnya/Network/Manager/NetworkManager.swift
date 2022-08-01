b7aa2a1704e705e7dce500637a3f7db4f4c78db1
chemi <wbsldj7645@naver.com>
2022-08-01 23:02:55 +0900

    [CHORE] NetworkManager 줄정리 (#70)

--- Hatnya/Network/Manager/NetworkManager.swift
+++ Hatnya/Network/Manager/NetworkManager.swift
@@ -12,9 +12,9 @@ import FirebaseFirestoreSwift
 import Foundation
 
 final class NetworkManager {
-    
+
     private let firestore = Firestore.firestore()
-    
+
     enum NetworkError: Error {
         case encodingError(Error)
         case decodingError(Error)
@@ -28,58 +28,58 @@ final class NetworkManager {
         let uid = UserDefaults.standard.string(forKey: "User") ?? ""
         var userId = ""
         var homeworkId = ""
-        
+
         firestore.collection("StudyGroup/\(studyId)/Members")
             .getDocuments { querySnapshot, _ in
-                guard let documents = querySnapshot?.documents else { return }
+            guard let documents = querySnapshot?.documents else { return }
 
-                documents.forEach { snapshot in
-                    guard let idData = snapshot.data()["uid"] as? String else { return }
-                    if uid == idData {
-                        userId = snapshot.documentID
-                        
-                        snapshot.reference.collection("Homeworks")
-                            .getDocuments { querySnapshot, _ in
-                                guard let documents = querySnapshot?.documents else { return }
-                                documents.forEach { snapshot in
-                                    guard let countData = snapshot.data()["count"] as? Int else { return }
-                                    if count == countData {
-                                        homeworkId = snapshot.documentID
-                                        completionHandler("StudyGroup/\(studyId)/Members/\(userId)/Homeworks/\(homeworkId)")
-                                    }
-                                }
+            documents.forEach { snapshot in
+                guard let idData = snapshot.data()["uid"] as? String else { return }
+                if uid == idData {
+                    userId = snapshot.documentID
+
+                    snapshot.reference.collection("Homeworks")
+                        .getDocuments { querySnapshot, _ in
+                        guard let documents = querySnapshot?.documents else { return }
+                        documents.forEach { snapshot in
+                            guard let countData = snapshot.data()["count"] as? Int else { return }
+                            if count == countData {
+                                homeworkId = snapshot.documentID
+                                completionHandler("StudyGroup/\(studyId)/Members/\(userId)/Homeworks/\(homeworkId)")
                             }
+                        }
                     }
                 }
             }
+        }
     }
-    
+
     func post<T: Encodable>(with data: T, path: String) {
         do {
             let encodeData = try Firestore.Encoder().encode(data)
-            
+
             firestore.document(path)
                 .setData(encodeData) { error in
-                    if let error = error {
-                        print(NetworkError.badURL(error))
-                    }
+                if let error = error {
+                    print(NetworkError.badURL(error))
                 }
+            }
         } catch {
             print(NetworkError.encodingError(error))
         }
     }
-    
+
     func get<T: Decodable>(for type: T.Type, path: String, completionHandler: @escaping (T) -> Void) {
         firestore.document(path)
             .addSnapshotListener { snapshot, error in
-                guard let document = snapshot else { return }
-                do {
-                    let data = try document.data(as: type)
-                    completionHandler(data)
-                } catch {
-                    print(NetworkError.decodingError(error))
-                }
+            guard let document = snapshot else { return }
+            do {
+                let data = try document.data(as: type)
+                completionHandler(data)
+            } catch {
+                print(NetworkError.decodingError(error))
             }
+        }
     }
-    
+
 }
